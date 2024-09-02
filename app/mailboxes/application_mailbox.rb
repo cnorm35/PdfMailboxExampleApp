0602de683bf3af55148e4c59a9e611f984a43cf5
Cody Norman <wcodynorman@gmail.com>
2024-09-02 14:46:42 -0600

    add support and sales inboxes for routing video

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,7 +1,9 @@
 class ApplicationMailbox < ActionMailbox::Base
+  routing /^support@/i => :support
+  routing /^sales@/i => :sales
+
   routing /pdf/i => :pdf
   routing /guess-my-age/i => :guess_my_age
   routing /csv/i => :csv
   routing /next-up/i => :next_up
-  # routing all: :pdf
 end
---
+++ app/mailboxes/sales_mailbox.rb
@@ -0,0 +1,4 @@
+class SalesMailbox < ApplicationMailbox
+  def process
+  end
+end
