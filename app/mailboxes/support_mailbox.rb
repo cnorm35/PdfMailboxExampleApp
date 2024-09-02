8bf297b4f0fb21aaaf1b61e69489112939613c2d
Cody Norman <wcodynorman@gmail.com>
2024-09-02 15:13:40 -0600

    add support mailbox ruby parsing example

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,4 +1,7 @@
 class SupportMailbox < ApplicationMailbox
   def process
+    user_id = mail.to.first.split('@').first.split('+').last
+    user = User.find_by(uuid: user_id)
+    # Handle the email with the extracted user information
   end
 end
